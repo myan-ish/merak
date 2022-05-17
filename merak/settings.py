6b2429b68f48b8fe6f1c31ea33dcba26f11f3c01
Manish Kunwar <ranaxmond@gmail.com>
2022-05-17 07:21:11 +0545

    Update settings.py

--- merak/settings.py
+++ merak/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-=tm=w2o3m07$_6_^radlk2x(tinsfz^&f*kp5j5s%6!n#+!6%(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
