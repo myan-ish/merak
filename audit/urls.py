483b77bb6eb37327d07ce4efc6590ba2da82adb4
Manish Kunwar <ranaxmond@gmail.com>
2022-07-07 12:41:49 +0545

    Update urls.py

--- audit/urls.py
+++ audit/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from audit.views import ExpenseViewSet
+from audit.views import ExpenseCategoryViewSet, ExpenseViewSet
 
 
 router = routers.DefaultRouter()
 
 router.register("expense", ExpenseViewSet)
+router.register("expense_category", ExpenseCategoryViewSet)
 
 urlpatterns = [
     path("", include(router.urls)),
